ad0dc0b028bf399f897f37517bb7dfaeb63a0c02
garimalande20 <garimalande20@gmail.com>
2022-04-22 18:36:03 +0530

    create help and support

--- src/pages/HelpSupport.js
+++ src/pages/HelpSupport.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function HelpSupport() {
+    const [formData, setFormData] = useState({name:'',email:'', mobile:'', message:''});
     useEffect(()=>{
         window.scrollTo({ top:0,left:0,behavior:'smooth'});
     },[])
@@ -22,19 +23,19 @@ function HelpSupport() {
                                     <div className='helpAndSupportInput'>
                                         <div className='titleDiv'>
                                             <label className='TitleHeading'>Name</label>
-                                            <input type="text" className='form-control inputOffCanvas' placeholder='Enter Name' required />
+                                            <input type="text" onChange={(e) => setFormData({ ...formData, name: e.target.value })} defaultValue={formData.name} className='form-control inputOffCanvas' placeholder='Enter Name' name="name" required />
                                         </div>
                                         <div className='categoryDiv'>
                                             <label className='TitleHeading'>Email</label>
-                                            <input type="text" className='form-control' placeholder='Enter Email' required />
+                                            <input type="text" className='form-control' name="email" placeholder='Enter Email' onChange={(e) => setFormData({ ...formData, email: e.target.value })} defaultValue={formData.email} required />
                                         </div>
                                         <div className='priorityDiv'>
                                             <label className='TitleHeading'>Mobile No.</label>
-                                            <input type="text" className='form-control' placeholder='Enter Mobile' required />
+                                            <input type="text" className='form-control' name="mobile" placeholder='Enter Mobile' onChange={(e) => setFormData({ ...formData, mobile: e.target.value })} defaultValue={formData.mobile} required />
                                         </div>
                                         <div className='messageDiv'>
                                             <label className="form-label">Message</label>
-                                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="5" placeholder='Message' required />
+                                            <textarea className="form-control" id="exampleFormControlTextarea1" onChange={(e) => setFormData({ ...formData, message: e.target.value })} defaultValue={formData.message} name="message" rows="5" placeholder='Message' required />
                                         </div>
                                         <button className='sendBtn'>Submit</button>
                                     </div>
