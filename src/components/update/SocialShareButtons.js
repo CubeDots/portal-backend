fe6bfe8ddbef2f598b231dc357df307f35c75f22
Sanjeev Vishwakarma <sanjeev@cubedots.com>
2022-05-27 13:19:28 +0530

    Share related changes

--- src/components/update/SocialShareButtons.js
+++ src/components/update/SocialShareButtons.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import HelmetMetaData from "../../components/HelmetMetaData"
+import Helmet from "../../components/HelmetMetaData"
 import {FacebookShareButton, FacebookIcon} from "react-share";
 class Appp extends Component {
    render() {
@@ -8,21 +8,22 @@ class Appp extends Component {
    return (
        <>
     {/* //  <div className={classes.root}> */}
-   <HelmetMetaData title={"dfkjfk"}
-          description={ 'this.state.blog.title + this.state.blog.author.name'}
-          image={"https://cuengine-portal.s3.eu-west-2.amazonaws.com/cusocials/featured_image/1653032988_offers%20for%20web%20alya-03.jpg"}
-      ></HelmetMetaData>
-   <FacebookShareButton
-     url={"https://ogp.me/"}
-     quote={"CampersTribe - World is yours to explore"}
-     hashtag="#camperstribe"
-     imageUrl={'https://cuengine-portal.s3.eu-west-2.amazonaws.com/cusocials/featured_image/1653032988_offers%20for%20web%20alya-03.jpg'}
-    height={"300px"}
-    width={"300px"}
-     //  className={classes.socialMediaButton}
-   >
-     <FacebookIcon size={36} />
-   </FacebookShareButton>
+
+    <Helmet>
+        <meta property="og:image" content="https://staging.cubedots.com/" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:image:alt" content="The Open Graph logo"></meta>
+    </Helmet>
+    <FacebookShareButton
+     url={"https://staging.cubedots.com/"}
+     quote={"Cubedots - World is yours to explore"}
+     hashtag="#Cubedots-Portal"
+     imageUrl={'https://staging.cubedots.com/'}
+    >
+      <FacebookIcon size={36} />
+    </FacebookShareButton>
 {/* //  </div> */}
 </>
   )};
