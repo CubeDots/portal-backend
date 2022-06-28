9dea38e2015749a4b51a0e33b417dd170bfbbb99
Umesh1234 <umesh@cubedots.com>
2022-06-28 12:13:03 +0530

    update folder chooser and social share

--- src/components/update/SocialShareButtons.js
+++ src/components/update/SocialShareButtons.js
@@ -17,34 +17,14 @@ import {
 } from 'react-share';
 
 //alert('calling...');
-const ImageUrll = "https://cuengine-portal.s3.eu-west-2.amazonaws.com/"
+// const ImageUrll = "https://cuengine-portal.s3.eu-west-2.amazonaws.com/"
 function SocialShareButtons(props) {
     let publicPath = process.env.PUBLIC_URL;
+    const second_url = "https://cuengine-portal.s3.eu-west-2.amazonaws.com/" + props.data.featured_image;
+    const image_url = "https://www.cubedots.com/cusocials/" + props.data.category + '/' + props.data.slug;
 
-    const image_url = "https://cuengine-portal.s3.eu-west-2.amazonaws.com/" + props.data.featured_image;
-    const [title , setTitle] = useState(props.data.title);
-    const [imageurl , setImageurl] = useState(image_url);
-    const [description , setDescription] = useState(props.data.long_description);
-
-    document.querySelector('meta[property="og:title"]').setAttribute("content", title);
-    document.querySelector('meta[property="og:image"]').setAttribute("content", imageurl);
-    document.querySelector('meta[property="og:url"]').setAttribute("content", 'https://staging.cubedots.com/');
-    document.querySelector('meta[property="og:description"]').setAttribute("content", description);
-
-    
-
-    const TitleClickHandler = (props) =>{
-        setTitle(props.data.title)
-    }
-
-    const ImageUrlHandler = () =>{
-        setImageurl(imageurl)
-    }
-
-    const DescritionHandler = (props) =>{
-        setDescription(props.data.long_description)
-    }
-
+    const phpurl = 'https://www.cubedots.com/social_share.php?title=';
+   
     if (!props.data)
         return "";
 
@@ -54,48 +34,31 @@ function SocialShareButtons(props) {
                 Share it
                 &nbsp;
                 <FacebookShareButton
-                    url={imageurl}
-                    // url={'imageUrl,whatsapp:https://www.cubedots.com/cusocials/offers'}
-                    onImageurl={ImageUrlHandler}
-                    onTitle={TitleClickHandler}
-                    onDescription={DescritionHandler}
-                    >
+                    url={ phpurl + props.data.title + '&desc=' + props.data.long_description+'&img='+ second_url+ '&callback=' +  image_url}>
                     <FacebookIcon size={32} round />
                 </FacebookShareButton>
                 &nbsp;
                 <LinkedinShareButton
-                    url={'https://www.cubedots.com/cusocials/offers'}
-                    quote={props.data.title}>
+                    url={ phpurl +props.data.title+ '&desc=' + props.data.long_description + '&img='+second_url + '&callback='+image_url}>
                     <LinkedinIcon size={32} round />
                 </LinkedinShareButton>
                 &nbsp;
                 <WhatsappShareButton
-                    url={publicPath + 'https://www.cubedots.com/cusocials/' + props.data.category + '/' + props.data.slug}
-                    // url={'imageUrl,whatsapp:https://www.cubedots.com/cusocials/offers'}
-                    imageUrl={image_url}>
+                    url={ phpurl +props.data.title+'&desc='+props.data.long_description+'&img='+second_url+'&callback='+image_url}>
                     <WhatsappIcon size={32} round />
                 </WhatsappShareButton>
                 &nbsp;
                 <TelegramShareButton
-                    url={publicPath + 'https://www.cubedots.com/cusocials/' + props.data.category + '/' + props.data.slug}
-                    title={props.data.title}>
+                    url={ phpurl +props.data.title+'&desc='+props.data.long_description+'&img='+second_url+'&callback='+image_url}>
                     <TelegramIcon size={32} round />
                 </TelegramShareButton>
                 &nbsp;
                 <EmailShareButton
-                    url={publicPath + 'https://www.cubedots.com/cusocials/' + props.data.category + '/' + props.data.slug}
-                    subject={props.data.title}
-                    body={props.data.small_description}>
+                   url={ phpurl +props.data.title+'&desc='+props.data.long_description+'&img='+second_url+'&callback='+image_url}>
                     <EmailIcon size={32} round />
                 </EmailShareButton>
-
             </div>
-           
-
         </>
     );
-
-
 }
-
 export default SocialShareButtons;
